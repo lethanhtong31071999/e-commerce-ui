6e6de4699565ab4c6dd0251308c08eb937ef39c0
Thanh Tong <lethanhtong31071999@gmail.com>
2021-08-19 14:04:46 +0700

    deal and email

--- main.js
+++ main.js
@@ -76,3 +76,56 @@ function handleFillterGallery(filter) {
     }
   }
 }
+
+// Highlight navbar
+window.addEventListener("scroll", handleHighlightNavbar);
+
+function handleHighlightNavbar() {
+  const arrivalElement = document.querySelector("#arrival");
+  const homeElement = document.querySelector("#home");
+  const featureElement = document.querySelector("#feature");
+  const galleryElement = document.querySelector("#gallery");
+  const dealElement = document.querySelector("#deal");
+  const navHeader2Elements = document.querySelector(
+    "header .header-2 .navbar ul"
+  ).children;
+  // Home
+  if (homeElement.offsetTop - 200 <= window.scrollY) {
+    for (const aTag of navHeader2Elements) {
+      aTag.firstElementChild.classList.remove("active");
+    }
+    navHeader2Elements[0].firstElementChild.classList.add("active");
+  }
+
+  // Arrival
+  if (arrivalElement.offsetTop - 200 <= window.scrollY) {
+    for (const aTag of navHeader2Elements) {
+      aTag.firstElementChild.classList.remove("active");
+    }
+    navHeader2Elements[1].firstElementChild.classList.add("active");
+  }
+
+  // Feature
+  if (featureElement.offsetTop - 200 <= window.scrollY) {
+    for (const aTag of navHeader2Elements) {
+      aTag.firstElementChild.classList.remove("active");
+    }
+    navHeader2Elements[2].firstElementChild.classList.add("active");
+  }
+
+  // Gallery
+  if (galleryElement.offsetTop - 200 <= window.scrollY) {
+    for (const aTag of navHeader2Elements) {
+      aTag.firstElementChild.classList.remove("active");
+    }
+    navHeader2Elements[3].firstElementChild.classList.add("active");
+  }
+
+  // Deal
+  if (dealElement.offsetTop - 200 <= window.scrollY) {
+    for (const aTag of navHeader2Elements) {
+      aTag.firstElementChild.classList.remove("active");
+    }
+    navHeader2Elements[4].firstElementChild.classList.add("active");
+  }
+}
